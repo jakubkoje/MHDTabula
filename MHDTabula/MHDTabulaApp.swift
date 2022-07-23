8148ad1cb867b1836c5d02c6e91ddb301a16b954
Jakub Jelínek <jakubkoje@gmail.com>
2022-07-23 18:13:39 +0200

    Initial Commit

---
+++ MHDTabula/MHDTabulaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MHDTabulaApp.swift
+//  MHDTabula
+//
+//  Created by Jakub Jelínek on 23/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MHDTabulaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
